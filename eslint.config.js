7c20289d313dc8e8b8273fb3d88e392f14b7cfbd
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-28 11:30:35 -0300

    feat: Adiciona setup inicial

---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginTypescript from "@typescript-eslint/eslint-plugin";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginImport from "eslint-plugin-import";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist", "node_modules", "eslint.d.ts"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      typescript: eslintPluginTypescript,
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport,
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "no-console": "warn",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
---
+++ eslint.d.ts
@@ -0,0 +1,4 @@
+declare module 'eslint-plugin-import' {
+  const eslintPluginImport: any;
+  export = eslintPluginImport;
+}
