edbcc7f4196159e91a3405bf7ae7b9e1ce328de6
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-31 09:31:59 -0300

    fix: Corrige indentação dos arquivos

--- src/styles/themes/breakpoints.ts
+++ src/styles/themes/breakpoints.ts
@@ -1,9 +1,9 @@
 const breakpoints = {
-    xs: '419px',
-    sm: '724px',
-    md: '1034px',
-    lg: '1109x',
-    xl: '1294px',
+  xs: '419px',
+  sm: '724px',
+  md: '1034px',
+  lg: '1109x',
+  xl: '1294px',
 };
 
 const setBreakpoint = (size: string) => `@media (max-width: ${size})`;
--- src/styles/themes/fontWeights.ts
+++ src/styles/themes/fontWeights.ts
@@ -1,7 +1,7 @@
 export const fontWeights = {
-    light: 300,
-    regular: 400,
-    medium: 500,
-    semi_bold: 600,
-    bold: 700,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semi_bold: 600,
+  bold: 700,
 };
