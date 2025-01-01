c81e134975952d88ba9b5b3024a29918428f2223
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-01 14:13:12 -0300

    feat: Adiciona fontes ao projeto

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,11 @@
+import ChirpMedium from './Chirp-Medium.woff2';
+import ChirpRegular from './Chirp-Regular.woff2';
+import ChirpBold from './Chirp-Bold.woff2';
+
+const fonts = {
+  bold: ChirpBold,
+  regular: ChirpRegular,
+  medium: ChirpMedium
+};
+
+export default fonts;
