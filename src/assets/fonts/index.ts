1b8d8dbda21f57a20c9917a7cece2b0940bba8d6
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-02 18:50:01 -0300

    feat: Adiciona fonte extra bold

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -1,8 +1,10 @@
 import ChirpMedium from './Chirp-Medium.woff2';
 import ChirpRegular from './Chirp-Regular.woff2';
 import ChirpBold from './Chirp-Bold.woff2';
+import ChirpHeavy from './Chirp-Heavy.woff2';
 
 const fonts = {
+  heavy: ChirpHeavy,
   bold: ChirpBold,
   regular: ChirpRegular,
   medium: ChirpMedium
