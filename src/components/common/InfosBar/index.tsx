3481c1459e1fd9e3d8e8e61c28056fdb9768edc8
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-03 14:28:27 -0300

    feat: Adiciona componente reutilizável InfosBar

---
+++ src/components/common/InfosBar/index.tsx
@@ -0,0 +1,36 @@
+import * as S from './styles';
+
+const InfosLinks = [
+  'Sobre',
+  'Baixe o aplicativo do X',
+  'Central de Ajuda',
+  'Termos de Serviço',
+  'Política de Privacidade',
+  'Política de cookies',
+  'Acessibilidade',
+  'Informações de anúncios',
+  'Blog',
+  'Carreiras',
+  'Recursos da marca',
+  'Publicidade',
+  'Marketing',
+  'X para Empresas',
+  'Desenvolvedores',
+  'Diretório',
+  'Configurações',
+  '© 2025 X Corp',
+];
+
+const InfosBar = () => {
+  return (
+    <S.Nav>
+      {InfosLinks.map((info) => (
+        <a href='#'>
+          {info}
+        </a>
+      ))}
+    </S.Nav>
+  );
+};
+
+export default InfosBar;
