491e4fe629951d69e2607293c29f83e8aa376511
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-11 11:35:01 -0300

    feat: Adiciona novas imagens

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,7 @@
 import LogoSVG from './logo.svg';
 import IconClose from './close_icon.svg';
+import ProfileIcon from './profile_icon.svg';
+import SearchIcon from './search_icon.svg';
+import HomeIcon from './home_icon.svg';
 
-export { IconClose, LogoSVG };
+export { IconClose, LogoSVG, HomeIcon, SearchIcon, ProfileIcon };
