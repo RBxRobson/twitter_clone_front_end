eb38152479a6c0f71c79cfc202df280f2ecedb21
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-18 17:10:35 -0300

    feat: Adiciona novos componentes estilizados

---
+++ src/styles/common/Overlay.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100dvh;
+  width: 100dvw;
+  background-color: #5b708366;
+`;
