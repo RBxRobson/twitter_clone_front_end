ca4ef222c7780a9e12b86934a289181e2fa875ab
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-23 15:36:06 -0300

    fix: Corrige tamanho da imagem

--- src/components/common/SmallAvatar/styles.ts
+++ src/components/common/SmallAvatar/styles.ts
@@ -2,7 +2,8 @@ import styled, { css } from 'styled-components';
 
 export const SmallAvatar = styled.img`
   ${({ theme }) => css`
-    width: 40px;
+    min-width: 40px;
+    max-width: 40px;
     height: 40px;
     border-radius: 50%;
     object-fit: cover;
