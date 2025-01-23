1ec0c8edcc0b3db6c8ca2148a6a968677b0d3a68
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-23 15:37:57 -0300

    feat: Melhorias de estilo

--- src/components/layouts/AsideInfos/styles.ts
+++ src/components/layouts/AsideInfos/styles.ts
@@ -9,12 +9,19 @@ export const AsideInfos = styled.aside`
   flex-direction: column;
   gap: 18px;
   padding-top: 8px;
-  padding-left: 30px;
-  padding-right: 8px;
+  margin-right: 12px;
+  margin-left: 18px;
+  max-width: 350px;
+  width: 100%;
+  text-align: start;
 
   &>nav {
     justify-content: start;
-    padding: 0 12px;
+    padding: 0 16px;
     gap: 6px 12px;
+  }
+
+  & > nav > :last-child {
+    flex-basis: 100%;
   }
 `;
