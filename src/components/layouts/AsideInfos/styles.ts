77b2fa11c98299083c59ac22dc55e516af9cde8e
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-24 15:18:40 -0300

    feat: Adiciona breakpoint no componente AsideInfos

--- src/components/layouts/AsideInfos/styles.ts
+++ src/components/layouts/AsideInfos/styles.ts
@@ -1,27 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const AsideInfos = styled.aside`
-  position: sticky;
-  top: 0; 
-  height: 100dvh; 
-  background-color: transparent;
-  display: flex;
-  flex-direction: column;
-  gap: 18px;
-  padding-top: 8px;
-  margin-right: 12px;
-  margin-left: 18px;
-  max-width: 350px;
-  width: 100%;
-  text-align: start;
+  ${({ theme }) => css`
+    position: sticky;
+    top: 0; 
+    height: 100dvh; 
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    padding-top: 8px;
+    margin-right: 12px;
+    margin-left: 18px;
+    max-width: 350px;
+    width: 100%;
+    text-align: start;
 
-  &>nav {
-    justify-content: start;
-    padding: 0 16px;
-    gap: 6px 12px;
-  }
+    &>nav {
+      justify-content: start;
+      padding: 0 16px;
+      gap: 6px 12px;
+    }
 
-  & > nav > :last-child {
-    flex-basis: 100%;
-  }
+    & > nav > :last-child {
+      flex-basis: 100%;
+    }
+
+    ${theme.setBreakpoint(theme.breakpoints.sm)} {
+      display: none;
+    }
+  `}
 `;
