a50d6ee99c0cdd00484ad89f0d2ec35b528f86c8
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-27 18:53:38 -0300

    feat: Adiciona componentes SVG

---
+++ src/components/svgComponents/QuoteIcon.tsx
@@ -0,0 +1,18 @@
+const QuoteIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      width={24}
+      height={24}
+      {...props}
+    >
+      <path
+        d="M14.23 2.854a2.504 2.504 0 013.54 0l3.38 3.378c.97.977.97 2.559 0 3.536L9.91 21H3v-6.914L14.23 2.854zm2.12 1.414a.492.492 0 00-.7 0L5 14.914V19h4.09L19.73 8.354c.2-.196.2-.512 0-.708l-3.38-3.378zM14.75 19l-2 2H21v-2h-6.25z"
+      />
+    </svg>
+  );
+};
+
+export default QuoteIcon;
