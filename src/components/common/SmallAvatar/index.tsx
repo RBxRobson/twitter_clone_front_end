592f334137fba564f008be6b449182f4d19a9256
RBxRobson <robsonnbarbosa0@gmail.com>
2025-01-29 14:50:49 -0300

    feat: Melhoria no componente SmallAvatar

--- src/components/common/SmallAvatar/index.tsx
+++ src/components/common/SmallAvatar/index.tsx
@@ -1,10 +1,20 @@
 import * as S from './styles';
 
 type Props = {
-  user: User;
+  user?: User;
+  postUser?: UserDetails
 };
 
-const SmallAvatar = ({ user }: Props) => {
+const SmallAvatar = ({ user, postUser }: Props) => {
+  if (postUser) {
+    return (
+      <S.SmallAvatar
+        src={`https://rbxrobson.pythonanywhere.com${postUser.avatar}`} 
+        alt={`Foto de perfil do ${postUser.username}`}
+      />
+    );
+  }
+
   return (
     <S.SmallAvatar
       src={`https://rbxrobson.pythonanywhere.com${user?.profile.avatar}`} 
