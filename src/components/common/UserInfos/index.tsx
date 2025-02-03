1532acedbc85887d29d735a822c382602f35b214
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-03 16:25:47 -0300

    feat: Adiciona melhorias no UserInfos

--- src/components/common/UserInfos/index.tsx
+++ src/components/common/UserInfos/index.tsx
@@ -1,15 +1,93 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { formatTimeAgo } from '../../../utils/functions/formatedTimeAgo';
 import * as S from './styles';
 
 type Props = {
-  user: User;
+  user?: User;
+  post?: Post;
+  navigateProfile?: boolean;
 };
 
-const UserInfos = ({ user }: Props) => {
+const UserInfos = ({ user, post, navigateProfile = false }: Props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleProfileClick = (
+    event: React.MouseEvent,
+    username: string
+  ) => {
+    event.stopPropagation();
+    const sanitizedUsername = username.replace('@', '');
+  
+    if (`/profile/${sanitizedUsername}` === location.pathname) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      navigate(`/profile/${sanitizedUsername}`);
+    }
+  };
+
+  const setContentUserInfos = () => {
+    if (post && navigateProfile) {
+      return (
+        <S.PostContainer>
+          <S.UserInfos>
+            <S.WrapperOverflow>
+              <a onClick={() => handleProfileClick}>
+                {post?.user_details.name}
+              </a>
+            </S.WrapperOverflow>
+          </S.UserInfos>
+          <S.UserInfos>
+            <S.WrapperOverflow>
+              <span>{post?.user_details.username}</span>
+            </S.WrapperOverflow>
+          </S.UserInfos>
+          <time>
+            <span className='divisor'>·</span>
+            {formatTimeAgo(post?.created_at)}
+          </time>
+        </S.PostContainer>
+      );
+    } else if (post) {
+      return (
+        <S.PostContainer>
+          <S.UserInfos>
+            <S.WrapperOverflow>
+              <h4>
+                {post?.user_details.name}
+              </h4>
+            </S.WrapperOverflow>
+          </S.UserInfos>
+          <S.UserInfos>
+            <S.WrapperOverflow>
+              <span>{post?.user_details.username}</span>
+            </S.WrapperOverflow>
+          </S.UserInfos>
+          <time>
+            <span className='divisor'>·</span>
+            {formatTimeAgo(post?.created_at)}
+          </time>
+        </S.PostContainer>
+      );
+    } else {
+      return (
+        <S.UserInfos>
+          <S.WrapperOverflow>
+            <h4>{user?.name}</h4>
+          </S.WrapperOverflow>
+          <S.WrapperOverflow>
+            <span>
+              {user?.username}
+            </span>
+          </S.WrapperOverflow>
+        </S.UserInfos>
+      );
+    };
+  };
+
   return (
-    <S.UserInfos>
-      <h4>{user?.name}</h4>
-      <span>{user?.username}</span>
-    </S.UserInfos>
+    setContentUserInfos()
   );
 };
 
--- src/components/common/UserInfos/styles.ts
+++ src/components/common/UserInfos/styles.ts
@@ -1,23 +1,57 @@
 import styled, { css } from 'styled-components';
 
+export const PostContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    gap: 4px;
+
+    span, time {
+        color: ${theme.colors.darkShades.d_45};
+        cursor: default;
+      }
+
+    .divisor {
+      padding: 0 4px;
+    }
+  `}
+`;
+
 export const UserInfos = styled.div`
   ${({ theme }) => css`
     display: table;
     table-layout: fixed;
-    width: 100%;
+    width: fit-content;
     text-align: start;
 
-    h4,
-    span {
+    span, time {
+      color: ${theme.colors.darkShades.d_45};
+      cursor: default;
+    }
+  `}
+`;
+
+export const WrapperOverflow = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSizes.sm};
+    display: block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+
+    a {
+      font-weight: ${theme.fontWeights.bold};
       font-size: ${theme.fontSizes.sm};
-      display: block;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      overflow: hidden;
+      color: ${theme.colors.whiteShades.w_40};
     }
 
-    span {
-      color: ${theme.colors.darkShades.d_45};
+    span, h4, a {
+      display: inline;
+      width: fit-content;
+    }
+
+    h4:hover,
+    a:hover {
+      text-decoration: underline;
     }
   `}
 `;
