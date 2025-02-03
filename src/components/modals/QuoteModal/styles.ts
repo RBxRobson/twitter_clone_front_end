71526917515251524e94074b86181c0ea87c0012
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-03 16:27:15 -0300

    feat: Adiciona componente QuoteModal

---
+++ src/components/modals/QuoteModal/styles.ts
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components';
+
+export const FormWrapper = styled.div`
+    ${({ theme }) => css`
+    display: flex;
+    gap: 12px;
+    padding-bottom: 40px;
+    border-bottom: 1px solid ${theme.colors.darkShades.d_55};
+  `}
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  width: 100%;
+`;
