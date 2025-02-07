bfd8d76cc15cab9f257ea41322a6a57bd058d35c
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-07 18:32:51 -0300

    feat: Adiciona novo componente EditModal

---
+++ src/components/modals/EditModal/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 12px 0 40px;
+  width: 100%;
+`;
+
+export const WrapperEdit = styled.div`
+  display: flex;
+  gap: 8px;
+`;
