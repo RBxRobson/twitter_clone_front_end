20efe48e1a99dba0612d66c3a75395c9b955839a
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-10 17:55:39 -0300

    feat: Ajusta componente utilizando novo hook useFollow

--- src/components/common/ButtonFollow/index.tsx
+++ src/components/common/ButtonFollow/index.tsx
@@ -1,58 +1,22 @@
-import { useState } from 'react';
-
 import * as S from './styles';
-import { useFollowUserMutation, useUnfollowUserMutation } from '../../../services/api';
+import { useFollow } from '../';
 
 type Props = {
-  userId: number,
+  userId: number;
+  isFollowing: boolean;
 };
 
-const ButtonFollow = ({ userId }: Props) => {
-  const [followingState, setFollowingState] = useState<{ [key: number]: boolean }>({});
-  const [loadingState, setLoadingState] = useState<{ [key: number]: boolean }>({});
-  const [followUser] = useFollowUserMutation();
-  const [unfollowUser] = useUnfollowUserMutation();
-
-  const handleClick = async () => {
-    const isCurrentlyFollowing = followingState[userId] || false;
-
-    // Atualiza o estado de carregamento para o usuário específico
-    setLoadingState((prev) => ({
-      ...prev,
-      [userId]: true,
-    }));
-
-    try {
-      if (isCurrentlyFollowing) {
-        await unfollowUser(userId);
-        setFollowingState((prev) => ({
-          ...prev,
-          [userId]: false,
-        }));
-      } else {
-        await followUser(userId);
-        setFollowingState((prev) => ({
-          ...prev,
-          [userId]: true,
-        }));
-      }
-    } finally {
-      // Remove o estado de carregamento após a ação
-      setLoadingState((prev) => ({
-        ...prev,
-        [userId]: false,
-      }));
-    }
-  };
+const ButtonFollow = ({ userId, isFollowing }: Props) => {
+  const { followingState, loading, toggleFollow } = useFollow(userId, isFollowing);
 
   return (
     <S.ButtonFollow
       type="button"
-      onClick={handleClick}
-      disabled={loadingState[userId]}
-      className={loadingState[userId] ? 'loading' : ''}
+      onClick={toggleFollow}
+      disabled={loading}
+      className={loading ? 'loading' : ''}
     >
-      {followingState[userId] ? 'Cancelar' : 'Seguir'}
+      {followingState ? 'Cancelar' : 'Seguir'}
     </S.ButtonFollow>
   );
 };
