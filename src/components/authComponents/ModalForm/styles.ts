877dec6b27995d1969080b1e27c5de2da3e016df
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-12 16:25:19 -0300

    feat: Ajustes no authComponents

--- src/components/authComponents/ModalForm/styles.ts
+++ src/components/authComponents/ModalForm/styles.ts
@@ -4,10 +4,11 @@ import {
   ButtonPrimary, 
   Modal, 
   ButtonClose, 
-  Overlay, 
+  Overlay,
+  Label
 } from '../../../styles/common';
 
-export { ButtonClose };
+export { ButtonClose, Label };
 
 export const ModalOverlay = styled(Overlay)`
   display: flex;
@@ -33,64 +34,6 @@ export const TitleForm = styled.h2`
     text-align: start;
     margin-bottom: 50px;
   `};
-`;
-
-export const Label = styled.label`
-  ${({ theme }) => css`
-    position: relative;
-    margin-bottom: 6px;
-    cursor: text;
-
-    span {
-      position: absolute;
-      top: 50%;
-      left: 8px;
-      transform: translateY(-50%);
-      transition: ease-in 100ms;
-      font-size: ${theme.fontSizes.md};
-      color: ${theme.colors.darkShades.d_50};
-    }
-
-    &:focus-within span {
-      color: ${theme.colors.blueShades.b_50};
-
-      &.error {
-        color: red;
-      }
-    }
-
-    &:focus-within span,
-    input:not(:placeholder-shown) + span {
-      top: 8px;
-      transform: none;
-      font-size: ${theme.fontSizes.sm};
-    }
-  `};
-`;
-
-export const Input = styled.input`
-  ${({ theme }) => css`
-    width: 100%;
-    max-width: 440px;
-    outline: none;
-    background-color: transparent;
-    border: 1px solid ${theme.colors.darkShades.d_50};
-    font-size: ${theme.fontSizes.md};
-    color: ${theme.colors.whiteShades.w_40};
-    border-radius: 4px;
-    padding: 28px 8px 8px;
-    z-index: 1;
-
-    &:focus {
-      border: 1px solid ${theme.colors.blueShades.b_50};
-      outline: 1px solid ${theme.colors.blueShades.b_50};
-    }
-
-    &.error {
-      border: 1px solid red;
-      outline: 1px solid red;
-    }
-  `}
 `;
 
 export const MessageError = styled.small`
