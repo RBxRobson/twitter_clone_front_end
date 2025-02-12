f9ded9a36504a37d5417d2e105c4264866e7883d
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-12 16:27:33 -0300

    feat: Adiciona novo componente ProfileContent

---
+++ src/components/profileComponents/ProfileContent/index.tsx
@@ -0,0 +1,59 @@
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+import { DateIcon } from '../../../assets/images';
+import { RootReducer } from '../../../store';
+import * as S from './styles';
+import { ButtonFollow } from '../../common';
+import { UpdateUserModal } from '../../modals';
+
+type Props = {
+  user: User;
+}
+
+const ProfileContent = ({ user }:Props) => {
+  const location = useLocation();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const { user: currentUser } = useSelector((state: RootReducer) => state.user);
+  const formatDate = (isoDate: string) => {
+    const date = new Date(isoDate);
+    const month = date.toLocaleString('pt-BR', { month: 'long' });
+    const year = date.getFullYear(); // Ano
+    return `Entrou em ${month.charAt(0).toUpperCase() + month.slice(1)} de ${year}`;
+  };
+
+  return (
+    <S.ProfileContent>
+      {currentUser?.id === user.id ? (
+        <>
+          <S.ButtonProfile onClick={() => setIsOpenModal(true)}>
+          Editar perfil
+          </S.ButtonProfile>
+          {isOpenModal &&  <UpdateUserModal closeModal={() => setIsOpenModal(false)}/>}
+        </>
+      ) : (
+        <ButtonFollow userId={user.id} isFollowing={user.is_following}/>
+      )}
+      <h2>{user.name}</h2>
+      <S.Username>{user.username}</S.Username>
+      {user.profile.bio !== '' && <S.Bio>{user.profile.bio}</S.Bio>}
+      <S.CreatedAt>
+        <img src={DateIcon} alt="Ícone de calendário"/>
+        {formatDate(user.created_at)}
+      </S.CreatedAt>
+      <S.CountersWrapper>
+        <S.Counter href={`${location.pathname}/following`}>
+          <b>{user.following_count}</b>
+          <span>Seguindo</span>
+        </S.Counter>
+        <S.Counter href={`${location.pathname}/followers`}>
+          <b>{user.followers_count}</b>
+          <span>Seguidores</span>
+        </S.Counter>
+      </S.CountersWrapper>
+    </S.ProfileContent>
+  );
+};
+
+export default ProfileContent;
---
+++ src/components/profileComponents/index.ts
@@ -0,0 +1,11 @@
+import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
+import ProfileHero from './ProfileHero';
+import ProfileFeed from './ProfileFeed';
+
+export { 
+  ProfileHeader, 
+  ProfileContent, 
+  ProfileHero,
+  ProfileFeed,
+};
