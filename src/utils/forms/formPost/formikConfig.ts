2cfc8efad3ef56fddb937bb2db305126d7b889b0
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-14 13:58:39 -0300

    feat: Melhorias no formulário de postagens

--- src/utils/forms/formPost/formikConfig.ts
+++ src/utils/forms/formPost/formikConfig.ts
@@ -1,8 +1,11 @@
 import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { useCreatePostMutation, useEditPostMutation } from '../../../services/api';
 import { closePublicationModal } from '../../../store/reducers/publicationModal';
+import { RootReducer } from '../../../store';
 
 export type FormPost = {
   postType: 'original' | 'quote' | 'repost';
@@ -10,12 +13,21 @@ export type FormPost = {
   original_post?: number
 }
 
-export const useFormPost = (publication?: Post): ExtendedFormikProps<FormPost> => {
+type Props = {
+  publication?: Post;
+  notModal?: boolean;
+}
+
+export const useFormPost = ({publication, notModal = false}:Props): ExtendedFormikProps<FormPost> => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { user } = useSelector((state: RootReducer) => state.user);
+  const username = user!.username.replace('@', '');
   const [createPost, { isLoading: loadingCreate }] = useCreatePostMutation();
   const [editPost, { isLoading: loadingEdit }] = useEditPostMutation();
-  const dispatch = useDispatch();
 
   const isLoading = loadingCreate || loadingEdit;
+  const tags = location.pathname.includes(username) ? ['UserPosts'] : ['Posts', 'UserPosts'];
 
   const formik = useFormik<FormPost>({
     initialValues: {
@@ -37,7 +49,8 @@ export const useFormPost = (publication?: Post): ExtendedFormikProps<FormPost> =
           // Chama a API para criar o post
           await editPost({
             postId: publication.id,
-            postData: {content : values.content}
+            postData: {content : values.content},
+            tags: tags
           }).unwrap();
         } else {
           // Chama a API para criar o post
@@ -45,11 +58,14 @@ export const useFormPost = (publication?: Post): ExtendedFormikProps<FormPost> =
             post_type: values.postType,
             content: values.content,
             original_post: values.original_post,
+            tags: tags
           }).unwrap();
         }
     
-        // Fecha o modal e navega para a página inicial
-        dispatch(closePublicationModal());
+        // Fecha o modal, se houver
+        if (!notModal) {
+          dispatch(closePublicationModal());
+        }
     
         // Reseta o formulário após o envio
         resetForm();
