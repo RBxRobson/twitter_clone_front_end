27665b6506f311d43dd19c6519211363d82e2a7c
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-14 13:56:34 -0300

    feat: Adiciona lista de postagens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,68 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { FormPost, HeaderHome } from '../../components/homeComponents';
+import { Loading, PostList } from '../../components/common';
+import { useGetPostsQuery, useGetUserFeedQuery } from '../../services/api';
+import { RootReducer } from '../../store';
 import * as S from './styles';
 
 const Home = () => {
+  const [isForYouActive, setIsForYouActive] = useState(true);
+  const [hasFetchedUserFeed, setHasFetchedUserFeed] = useState(false);
+  const { user } = useSelector((state: RootReducer) => state.user);
+
+  const { data: userFeedData, isLoading: loadingUserFeed } = useGetUserFeedQuery(undefined, {
+    skip: isForYouActive && !hasFetchedUserFeed,
+  });
+
+  const { data: generalFeedData, isLoading: loadingGeneralFeed } = useGetPostsQuery();
+
+  const handleClickFollowing = () => {
+    setIsForYouActive(false);
+    if (!hasFetchedUserFeed) {
+      setHasFetchedUserFeed(true);
+    }
+  };
+
+  if (loadingUserFeed || loadingGeneralFeed) {
+    return (
+      <S.CentralWrapper>
+        <HeaderHome 
+          isForYouActive={isForYouActive}
+          onClickFollowing={handleClickFollowing}
+          onClickForYou={() => setIsForYouActive(true)}
+        />
+        <FormPost />
+        <S.LoadingContainer>
+          <Loading />
+        </S.LoadingContainer>
+      </S.CentralWrapper>
+    );
+  }
+
   return (
     <S.CentralWrapper>
-      <h2>Home</h2>
+      <HeaderHome 
+        isForYouActive={isForYouActive}
+        onClickFollowing={handleClickFollowing}
+        onClickForYou={() => setIsForYouActive(true)}
+      />
+      <FormPost />
+      {isForYouActive 
+        ? <PostList posts={generalFeedData!}/> 
+        : (
+          <>
+            <PostList posts={userFeedData!}/>
+            {userFeedData?.length === 0 
+            && user?.following_count === 0
+            && 'Você ainda não segue ninguém.'}
+            {userFeedData?.length === 0 
+            && user?.following_count! >= 0
+            && 'Seus seguidores não possuem postagens no momento'}
+          </>
+        )
+      }
     </S.CentralWrapper>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,3 +3,11 @@ import styled, { css } from 'styled-components';
 import { CentralWrapper } from '../../styles/common';
 
 export { CentralWrapper };
+
+export const LoadingContainer = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    padding-top: 12px;
+    border-top: 1px solid ${theme.colors.darkShades.d_55};
+  `}
+`;
