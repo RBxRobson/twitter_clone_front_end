9620461aca7f1200a968db3062233d1025d2b75a
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-17 10:16:09 -0300

    feat: Adiciona novo componente estilizado HeaderBlur

---
+++ src/styles/common/HeaderBlur.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderBlur = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background: rgba(0, 0, 0, 0.76);
+  backdrop-filter: blur(6px);
+`;
