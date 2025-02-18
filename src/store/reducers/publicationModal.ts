8a0414a34e874be33cf113374389f8258d55f097
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-18 18:20:44 -0300

    refactor: Altera reducer para atender as alterações no modelo de Post

--- src/store/reducers/publicationModal.ts
+++ src/store/reducers/publicationModal.ts
@@ -1,19 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { Comment } from '../../types/comment-details';
-
 type PublicationModalState = {
   isOpen: boolean;
   typeModal: 'comment' | 'post' | 'quote' | 'edit' | null;
   post: Post | null;
-  comment: Comment | null;
 };
 
 const initialState: PublicationModalState = {
   isOpen: false,
   typeModal: null,
   post: null,
-  comment: null
 };
 
 const publicationModalSlice = createSlice({
@@ -29,39 +25,21 @@ const publicationModalSlice = createSlice({
     closePublicationModal: (state) => {
       state.isOpen = false;
       state.typeModal = null;
-      state.comment = null;
       state.post = null;
     },
     openQuoteModal: (state, action: PayloadAction<Post>) => {
       state.typeModal = 'quote';
       state.post = action.payload;
-      state.comment = null;
       state.isOpen = true;
     },
-    openCommentModal: (state, action: PayloadAction<Comment | Post>) => {
+    openCommentModal: (state, action: PayloadAction<Post>) => {
       state.typeModal = 'comment';
-
-      if ('post_type' in action.payload) {
-        state.post = action.payload as Post;
-        state.comment = null;
-      } else {
-        state.comment = action.payload as Comment;
-        state.post = null;
-      }
-      
+      state.post = action.payload;
       state.isOpen = true;
     },
-    openEditModal: (state, action: PayloadAction<Comment | Post>) => {
+    openEditModal: (state, action: PayloadAction<Post>) => {
       state.typeModal = 'edit';
-
-      if ('post_type' in action.payload) {
-        state.post = action.payload as Post;
-        state.comment = null;
-      } else {
-        state.comment = action.payload as Comment;
-        state.post = null;
-      }
-      
+      state.post = action.payload;
       state.isOpen = true;
     },
   },
