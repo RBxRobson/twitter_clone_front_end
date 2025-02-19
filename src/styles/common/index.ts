7b2dfad2c407462de5804962d0a8cb09c447b6d8
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-19 14:33:50 -0300

    feat: Remove importação

--- src/styles/common/index.ts
+++ src/styles/common/index.ts
@@ -3,7 +3,6 @@ import { Modal } from './Modal';
 import { ButtonClose } from './ButtonClose';
 import { Overlay } from './Overlay';
 import { CentralWrapper } from './CentralWrapper';
-import { BtnBack } from './ButtonBack';
 import { TextField } from './TextField';
 import { PopUp } from './PopUp';
 import { Label } from './Label';
@@ -15,7 +14,6 @@ export {
   Modal,
   Overlay,
   CentralWrapper,
-  BtnBack,
   TextField,
   PopUp,
   Label,
