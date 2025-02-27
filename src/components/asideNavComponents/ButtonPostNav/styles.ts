a7302197c3a96c40738c310ec11ee0f2506aa7d5
RBxRobson <robsonnbarbosa0@gmail.com>
2025-02-27 17:05:13 -0300

    feat: Ajustes responsivos no botão de publicação

--- src/components/asideNavComponents/ButtonPostNav/styles.ts
+++ src/components/asideNavComponents/ButtonPostNav/styles.ts
@@ -1,9 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { ButtonPrimary } from '../../../styles/common';
+import { setBreakpoint } from '../../../styles/themes/breakpoints';
 
 export const ButtonPostNav = styled(ButtonPrimary)`
-  margin-bottom: 20px;
-  margin-top: auto;
-  padding: 16px;
+  ${({ theme }) => css`
+    margin-bottom: 20px;
+    margin-top: auto;
+    padding: 16px;
+
+    span {
+      font-size: ${theme.fontSizes.md};
+    }
+
+    img {
+      display: none;
+      width: 24px;
+    }
+
+    ${setBreakpoint(theme.breakpoints.xl)} {
+      width: fit-content;
+
+      span {
+        display: none;
+      }
+
+      img {
+        display: block;
+      }
+    }
+  `};
 `;
