532a2d4c891e792c51b6e0b569d5f026409e3bb3
RBxRobson <robsonnbarbosa0@gmail.com>
2025-03-10 14:58:38 -0300

    feat: Ajuste na responsividade do WrapperCentral

--- src/styles/common/CentralWrapper.ts
+++ src/styles/common/CentralWrapper.ts
@@ -1,13 +1,20 @@
 import styled, { css } from 'styled-components';
 
+import { setBreakpoint } from '../themes/breakpoints';
+
 export const CentralWrapper = styled.div`
   ${({ theme }) => css`
     width: 100%;
+    max-width: 650px;
     margin-right: 12px;
     border: 1px solid ${theme.colors.darkShades.d_55};
     border-top: none;
     border-bottom: none;
     position: relative;
     z-index: 1;
+
+    ${setBreakpoint(theme.breakpoints.sm)} {
+      margin-right: 4px;
+    }
   `}
 `;
